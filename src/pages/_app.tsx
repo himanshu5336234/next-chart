2827fd73b3aae339f4929290b8b4a0ebec598f4e
himanshu5336234 <himanshu5336234@gmail.com>
2024-06-27 20:52:24 +0530

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from 'next/script';
 export default function App({ Component, pageProps }: AppProps) {
   return<>
-
+   <Script src="charting_library/charting_library.js"></Script>
   <Component {...pageProps} />
   </>;
 }
