c8ee1d8f6fec56d2613c5e9ada6e7c723668e174
himanshu5336234 <himanshu5336234@gmail.com>
2024-06-28 00:18:02 +0530

    fix

--- src/TradingViewChart/TradingViewChart/helpers.ts
+++ src/TradingViewChart/TradingViewChart/helpers.ts
@@ -30,25 +30,35 @@ export const widgetContainer = {
   allow_symbol_change: true,
   theme: "dark",
   timezone: "Asia/Kolkata",
-  library_path: "/charting_library/charting_library.js",
+  library_path: "/chart/charting_library.js",
   custom_css_url: "css/style.css",
-  // debug:true,
-  disabled_features: ["context_menus", "use_localstorage_for_settings", "header_saveload", "header_symbol_search", "symbol_search_hot_key"],
-  enabled_features: ["study_templates", "hide_left_toolbar_by_default", "header_chart_type", "show_symbol_logos", "hide_right_toolbar"],
-  toolbar_bg: "#1B1B1F",
+  custom_font_family:'"Text-Medium", sans-serif',
+  disabled_features: [
+    "context_menus",
+    "use_localstorage_for_settings",
+    "header_saveload",
+    "header_symbol_search",
+    "symbol_search_hot_key",
+  ],
+  enabled_features: [
+    "study_templates",
+    "hide_left_toolbar_by_default",
+    "header_chart_type",
+    "show_symbol_logos",
+    "hide_right_toolbar",
+  ],
+  toolbar_bg: "#101019",
   loading_screen: {
-    backgroundColor: "#1B1B1F",
-    foregroundColor: "#E2FF6F"
+    backgroundColor: "#101019",
+      foregroundColor:   "#101019",
   },
   overrides: {
     "paneProperties.backgroundType": "solid",
-    "paneProperties.background": "#1B1B1F",
-    // "paneProperties.textColor": "#101010",
+    "paneProperties.background": "#101019",
+    "paneProperties.textColor": "#ffffff",
     "paneProperties.vertGridProperties.color": "#222225",
-    "paneProperties.horzGridProperties.color": "#222225",
-    "paneProperties.bottomMargin": 1,
-    "paneProperties.topMargin": 1,
-    // "paneProperties.crossHairProperties.color": "#A9A9A9",
+    "paneProperties.horzGridProperties.color" : "#222225",
+    "paneProperties.crossHairProperties.color": "#A9A9A9",
     "mainSeriesProperties.candleStyle.borderUpColor": "#29B57E",
     "mainSeriesProperties.candleStyle.borderDownColor": "#F46151",
     "mainSeriesProperties.candleStyle.borderColor": "#29B57E",
@@ -57,13 +67,10 @@ export const widgetContainer = {
     "mainSeriesProperties.candleStyle.wickColor": "#29B57E",
     "mainSeriesProperties.candleStyle.wickUpColor": "#29B57E",
     "mainSeriesProperties.candleStyle.wickDownColor": "#F46151",
-
-    "scalesProperties.textColor": "#D8D8DD",
-    "scalesProperties.fontSize": 11,
+    "scalesProperties.textColor": "white",
     "scalesProperties.backgroundColor": "#171717",
-    "scalesProperties.showBidAskLabels": true
-    // "symbolWatermarkProperties.color": "rgba(0, 0, 0, 0.00)",
-    // "symbolWatermarkProperties.visibility": false
+    "symbolWatermarkProperties.color": "rgba(0, 0, 0, 0.00)",
+    "symbolWatermarkProperties.visibility": false,
   }
 };
 export const configurationData = {
